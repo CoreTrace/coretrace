6716db976af74fb9a0e0b5fba25a0aab933ed785
Hugo <hugo.payet@epitech.eu>
2026-01-22 01:45:27 +0100

    chore(app): add config module header

---
+++ include/App/Config.hpp
@@ -0,0 +1,12 @@
+#ifndef APP_CONFIG_HPP
+#define APP_CONFIG_HPP
+
+#include "Config/config.hpp"
+#include "attributes.hpp"
+
+namespace ctrace
+{
+    CT_NODISCARD ProgramConfig buildConfig(int argc, char *argv[]);
+}
+
+#endif // APP_CONFIG_HPP
