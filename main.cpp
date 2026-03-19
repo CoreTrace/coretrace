48125fb8c48bf288d10b27eef3efa115715a0fe1
Hugo <hugo.payet@epitech.eu>
2026-03-19 15:07:54 +0900

    refactor(main): initialize coretrace logger before execution

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
 #include "App/Config.hpp"
 #include "App/Runner.hpp"
-#include "ctrace_tools/colors.hpp"
 
-#include <iostream>
+#include <coretrace/logger.hpp>
 
 int main(int argc, char* argv[])
 {
     ctrace::ProgramConfig config = ctrace::buildConfig(argc, argv);
 
-    std::cout << ctrace::Color::GREEN << "CoreTrace - Comprehensive Tracing and Analysis Tool"
-              << ctrace::Color::RESET << std::endl;
+    // std::cout << ctrace::Color::GREEN << "CoreTrace - Comprehensive Tracing and Analysis Tool"
+    //           << ctrace::Color::RESET << std::endl;
+
+    coretrace::enable_logging();
+    coretrace::set_prefix("== CoreTrace ==");
+    coretrace::set_min_level((config.global.verbose) ? coretrace::Level::Debug
+                                                     : coretrace::Level::Info);
+    coretrace::set_source_location(false);
+    coretrace::set_thread_safe(false);
 
     if (config.global.ipc == "serve")
     {
+        coretrace::set_timestamps(true);
         return ctrace::run_server(config);
     }
     return ctrace::run_cli_analysis(config);
