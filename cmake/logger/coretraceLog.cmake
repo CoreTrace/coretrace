5303761cf5b2cc50bbf184680832e02941e25921
Hugo <hugo.payet@epitech.eu>
2026-05-21 18:58:48 +0900

    fix(cmake): deduplicate coretrace logger dependency

--- cmake/logger/coretraceLog.cmake
+++ cmake/logger/coretraceLog.cmake
@@ -1,4 +1,15 @@
 # SPDX-License-Identifier: Apache-2.0
+include_guard(GLOBAL)
+
+if(TARGET coretrace::logger)
+  return()
+endif()
+
+if(TARGET coretrace_logger)
+  add_library(coretrace::logger ALIAS coretrace_logger)
+  return()
+endif()
+
 set(CORETRACE_LOGGER_BUILD_EXAMPLES OFF CACHE BOOL "Disable logger examples" FORCE)
 set(CORETRACE_LOGGER_BUILD_TESTS OFF CACHE BOOL "Disable logger tests" FORCE)
 
